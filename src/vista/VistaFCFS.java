d89ac3a300426dec4c3d239c18374fa5a9ec4ee8
CCaroV <51457651+CCaroV@users.noreply.github.com>
2021-09-17 22:35:36 -0500

    Add files via upload
    My first readme

---
+++ src/vista/VistaFCFS.java
@@ -0,0 +1,112 @@
+package vista;
+
+import java.awt.HeadlessException;
+import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
+
+/**
+ *
+ * @author Christian Caro Vargas
+ * @author Jorge Andrés Bohórquez Castellanos
+ * @author Santiago Ríos Valero
+ */
+public class VistaFCFS extends JFrame {
+
+    //Definición de páneles
+    private final JPanel panelTables;
+    private final JPanel panelGraphs;
+
+    //Definición de labels
+    private final JLabel[] lblProcess;
+
+    private JScrollPane scroll = new JScrollPane();
+
+    //Definición de botones
+    private final JButton[] btnAdd;
+    private final JButton[] btnExec;
+    private final JButton[] btnTerminate;
+
+    //Definición de tablas
+    private JTable tblProcesses;
+
+    public VistaFCFS() throws HeadlessException {
+        //Título
+        super("Primero en llegar primero en salir");
+
+        //Parámetros del Frame
+        this.setSize(1270, 1000);
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        this.setLayout(null);
+        this.setLocationRelativeTo(null);
+
+        //Constructor de páneles
+        panelTables = new JPanel();
+        panelGraphs = new JPanel();
+        this.add(panelGraphs);
+        this.add(panelTables);
+        panelTables.setBounds(10, 10, 1260, 300);
+        panelTables.setLayout(null);
+
+        //Constructor de labels
+        lblProcess = new JLabel[5];
+        for (int i = 0; i < 5; i++) {
+            lblProcess[i] = new JLabel("Proceso " + String.valueOf(i + 1));
+            panelTables.add(lblProcess[i]);
+        }
+        lblProcess[0].setBounds(10, 30, 100, 30);
+        lblProcess[1].setBounds(10, 70, 100, 30);
+        lblProcess[2].setBounds(10, 110, 100, 30);
+        lblProcess[3].setBounds(10, 150, 100, 30);
+        lblProcess[4].setBounds(10, 190, 100, 30);
+
+        //Constructor de botones
+        btnAdd = new JButton[5];
+        btnExec = new JButton[5];
+        btnTerminate = new JButton[5];
+        for (int i = 0; i < 5; i++) {
+            btnAdd[i] = new JButton("Añadir");
+            panelTables.add(btnAdd[i]);
+
+            btnExec[i] = new JButton("Ejecutar");
+            panelTables.add(btnExec[i]);
+
+            btnTerminate[i] = new JButton("Terminar");
+            panelTables.add(btnTerminate[i]);
+        }
+        btnAdd[0].setBounds(120, 30, 90, 30);
+        btnAdd[1].setBounds(120, 70, 90, 30);
+        btnAdd[2].setBounds(120, 110, 90, 30);
+        btnAdd[3].setBounds(120, 150, 90, 30);
+        btnAdd[4].setBounds(120, 190, 90, 30);
+
+        btnExec[0].setBounds(230, 30, 100, 30);
+        btnExec[1].setBounds(230, 70, 100, 30);
+        btnExec[2].setBounds(230, 110, 100, 30);
+        btnExec[3].setBounds(230, 150, 100, 30);
+        btnExec[4].setBounds(230, 190, 100, 30);
+
+        btnTerminate[0].setBounds(350, 30, 100, 30);
+        btnTerminate[1].setBounds(350, 70, 100, 30);
+        btnTerminate[2].setBounds(350, 110, 100, 30);
+        btnTerminate[3].setBounds(350, 150, 100, 30);
+        btnTerminate[4].setBounds(350, 190, 100, 30);
+
+        //Definición de tablas
+        tblProcesses = new JTable();
+        panelTables.add(tblProcesses);
+        tblProcesses.setBounds(500, 10, 400, 400);
+        tblProcesses.setModel(new DefaultTableModel(
+                new Object[][]{
+                    {null, null, null, null},
+                    {null, null, null, null},
+                    {null, null, null, null},
+                    {null, null, null, null}
+                },
+                new String[]{
+                    "Title 1", "Title 2", "Title 3", "Title 4"
+                }
+        ));
+
+    }
+
+}
