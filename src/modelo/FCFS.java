4142b58f3879fedc59c34829642014598a7a519e
CCaroV <chcarov@correo.udistrital.edu.co>
2021-10-10 18:56:59 -0500

    Get del texto en el modelo agregado

--- src/modelo/FCFS.java
+++ src/modelo/FCFS.java
@@ -14,5 +14,12 @@ public final class FCFS {
         this.text = new Object[6][5];
 
     }
-
+    
+    public Object getText(int row, int column) {
+        return this.text[row][column];
+    }
+    
+    public void setText(int row, int column, Object object) {
+        this.text[row][column] = object;
+    }
 }
