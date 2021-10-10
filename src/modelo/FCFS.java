21faf6e585063eaa5788ba4f93b64ce48c7bbf82
CCaroV <chcarov@correo.udistrital.edu.co>
2021-10-10 16:08:01 -0500

    Modelo agregado

--- src/modelo/FCFS.java
+++ src/modelo/FCFS.java
@@ -1,7 +1,5 @@
 package modelo;
 
-import java.util.*;
-
 /**
  *
  * @author Christian Caro Vargas
@@ -10,36 +8,11 @@ import java.util.*;
  */
 public final class FCFS {
 
-    public Timer timer;
     private final Object[][] text;
-    private int seconds;
 
     public FCFS() {
         this.text = new Object[6][5];
-        this.timer = new Timer();
-        this.seconds = 0;
 
-        seconds();
-    }
-
-    TimerTask tarea = new TimerTask() {
-        @Override
-        public void run() {
-            seconds++;
-            System.out.println("Segundo: " + seconds);
-        }
-    };
-
-    public void seconds() {
-        //timer.schedule(tarea, 0, 1000);
-    }
-
-    public int getSeconds() {
-        return seconds;
-    }
-
-    public void setSeconds(int seconds) {
-        this.seconds = seconds;
     }
 
 }
